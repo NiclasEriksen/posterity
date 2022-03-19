5d7500e09d3c72da233ca3e6cb31b76debc0d69b
Niclas Eriksen <niclaseriksen@gmx.com>
2022-03-19 23:53:34 +0100

    Dynamic audio normalization, lower peak

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -2,6 +2,7 @@ from celery.utils.log import get_task_logger
 
 from app import celery
 from app.dl.dl import download_from_json_data
+from app.dl.helpers import seconds_to_time
 
 logger = get_task_logger(__name__)
 
@@ -16,9 +17,12 @@ def test_task():
 @celery.task(name="core.tasks.download", soft_time_limit=5400, time_limit=7200)
 def download_task(data: dict, file_name: str):
     import time
-    logger.info("Starting download.")
+    dur = time.time()
+    logger.info("Starting download of " + str(file_name))
     success = download_from_json_data(data, file_name)
+    dur = time.time() - dur
+    _hours, minutes, seconds = seconds_to_time(dur)
     if success:
-        logger.info("Download complete.")
+        logger.info(f"Download complete in {minutes} minutes, {seconds:.0f} seconds: {file_name}")
     else:
-        logger.error("Download failed.")
+        logger.error(f"Download failed after {minutes} minutes, {seconds:.0f} seconds: {file_name}")
