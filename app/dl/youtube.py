6e5cd4bc5a0e283e64494fe45072838111f565a6
Niclas Eriksen <niclaseriksen@gmx.com>
2022-04-12 22:42:33 +0200

    Strip useless words.

--- app/dl/youtube.py
+++ app/dl/youtube.py
@@ -83,7 +83,8 @@ AD_DESCRIPTIONS = [
     "in-depth channel:", "auto-generated", "breaking news videos", "read the sun", "____", "to read this story",
     "facebook:", "twitter:", "instagram:", "tumblr:", "news for more", "news here:", "more videos from",
     "with the latest news", "my instagram", "my facebook", "my twitter", "my youtube", "contact me",
-    "thank you for your", "paypal", "with latest headlines", "sports and entertainment", "also watch"
+    "thank you for your", "paypal", "with latest headlines", "sports and entertainment", "also watch",
+    "most watched"
 ]
 
 
