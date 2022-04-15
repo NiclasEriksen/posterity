42fab80b681e19cf91d55acdb0f609f524bd0902
Niclas Eriksen <niclaseriksen@gmx.com>
2022-04-15 03:50:22 +0200

    Just desc in desc

--- app/serve/views.py
+++ app/serve/views.py
@@ -258,7 +258,7 @@ def edit_video_page(video_id: str):
     if not video.user_can_edit(current_user):
         flash("Lacking permissions to edit that video.", "error")
         return serve_video(video.video_id)
-    if not video.can_be_changed:
+    if video.status == STATUS_DOWNLOADING:
         flash("Can't edit video right now, sorry.", "warning")
         return serve_video(video.video_id)
 
